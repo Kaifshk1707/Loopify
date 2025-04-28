f2d1b03c2f73b912b4fc0c120ec288abf4d1008a
Asad Shaikh <shaikhasd20@gmail.com>
2025-04-28 18:21:35 +0530

    Create new.

---
+++ src/screens/Notification/Notificationscreen.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Notificationscreen = () => {
+  return (
+    <View>
+      <Text>Notificationscreen</Text>
+    </View>
+  )
+}
+
+export default Notificationscreen
