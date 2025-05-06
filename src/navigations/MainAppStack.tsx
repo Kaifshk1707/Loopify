15f56c03f570c8f8dddafee308ad341af1d08a23
Asad Shaikh <shaikhasd20@gmail.com>
2025-05-06 14:20:42 +0530

    apk

--- src/navigations/MainAppStack.tsx
+++ src/navigations/MainAppStack.tsx
@@ -1,8 +1,6 @@
 import { createStackNavigator } from "@react-navigation/stack";
 import AuthStack from "./AuthStack";
-import { useEffect, useState } from "react";
 import MainAppBottomTab from "./MainBottomTab";
-import HomeScreen from "../screens/Home/HomeScreen";
 import ChatListScreen from "../components/Chats/ChatListScreen";
 import ChatScreen from "../components/Chats/ChatScreen";
 import CreatePostScreen from "../components/Posts/CreatePostScreen";
@@ -15,7 +13,7 @@ const MainAppStack = () => {
       // initialRouteName={"AuthStack"}
       screenOptions={{ headerShown: false }}
     >
-      {/* <Stack.Screen name="AuthStack" component={AuthStack} /> */}
+      <Stack.Screen name="AuthStack" component={AuthStack} />
       <Stack.Screen name="MainAppBottomTab" component={MainAppBottomTab} />
       <Stack.Screen name="ChatListScreen" component={ChatListScreen} />
       <Stack.Screen name="ChatScreen" component={ChatScreen} />
